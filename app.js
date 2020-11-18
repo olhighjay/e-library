33083c027a84c18e2d4884bdffe86515c0994b9c
Olhighjay <olhighjayartrage@gmail.com>
2020-11-18 17:20:52 +0100

    serving HTML

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/', )));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css' )));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js' )));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jqury/dist' )));
+
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+});
+
+
+app.listen(3000, () => {
+  debug(`Server is running on ${chalk.green('3000')}`);
+});
